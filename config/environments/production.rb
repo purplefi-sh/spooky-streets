175c68feabfaf1402903c589a5a74bfb862b5b2c
Sergio Pantoja <2530982+sergiopantoja@users.noreply.github.com>
2020-09-19 17:35:06 -0500

    Update app name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "quickstart_production"
+  # config.active_job.queue_name_prefix = "spooky_production"
 
   config.action_mailer.perform_caching = false
 
